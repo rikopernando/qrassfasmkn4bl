2089b52da1f46d5759240df4599c5cf24757b862
hendriandaglos <baifernsh25>
2016-12-16 13:19:19 +0700

    untuk tampilan navbar, hanya menambahkan link laporan, agar bisa di ambil dan tidak mengalami EROR

--- navbar.php
+++ navbar.php
@@ -307,13 +307,18 @@ if ($lihat['akuntansi_lihat'] > 0){
                         </div>
                     </li>';
 }
-/*
+
  if ($lihat['laporan_lihat'] > 0){
                 echo '<li><a class="collapsible-header waves-effect arrow-r"><i class="fa fa-book"></i> Laporan <i class="fa fa-angle-down rotate-icon"></i></a>
                         <div class="collapsible-body">
                             <ul>';
 }
 
+echo"<li><a href=laporan_tabungan_rekening.php class='waves-effect'>Rekening</a></li>";
+
+echo"<li><a href=laporan_tabungan_jurusan.php class='waves-effect'>Jurusan</a></li>";
+
+/*
  if ($lihat['laporan_penjualan_lihat'] > 0){
                             echo '<li><a href="lap_penjualan.php" class="waves-effect">Lap. Penjualan</a></li>';
 }
@@ -357,6 +362,7 @@ if ($lihat['akuntansi_lihat'] > 0){
  if ($lihat['laporan_komisi_faktur_lihat'] > 0){
                             echo '<li><a href="laporan_fee_faktur.php" class="waves-effect">Lap. Komisi / Faktur </a></li>';
 }
+*/
 
 if ($lihat['laporan_lihat'] > 0){
                         echo '</ul>
@@ -364,7 +370,7 @@ if ($lihat['laporan_lihat'] > 0){
                     </li>';
 }
 
-*/
+
  if ($lihat['kas_lihat'] > 0){
                 echo '<li><a href="kas.php" class="waves-effect"> <i class="fa fa-money"></i> Posisi Kas </a></li>';
 }
